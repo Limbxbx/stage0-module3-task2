9433cfddcf5085aa65a5a37f7137e775b7f4016a
limbxbx <ilya.msd2003@gmail.com>
2023-01-25 00:05:33 +0200

    refactor

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+    public static final int adult_age = 18;
 
-    private int AGE;
+    private final int age = adult_age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
+
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber() {
+        int number = phoneNumber;
     }
 }
