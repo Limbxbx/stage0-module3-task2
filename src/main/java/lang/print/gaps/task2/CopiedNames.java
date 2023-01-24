1d11636aefc0bc1cf362217da0eb1140de35669f
limbxbx <ilya.msd2003@gmail.com>
2023-01-25 01:45:38 +0200

    refactor

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,14 +3,10 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        variable = 2000;
-        int value = 1000;
-        variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        value = 90000;
+        int value;
+        int value2;
+        int variables;
         int my_integer = 1000000;
         int my_var = 1234567;
-        my_integer = 1234567;    }
+    }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        int variable;
+        int variable_One;
+        int variableTwo;
+        int variable_three;
+        final int variableFour;
+        final int variableFive;
+        int variableSix;
+        int variableSeven;
+        int variableEight;
     }
 }
