ed962dd802364b8772ef843aad23b151cca447ce
limbxbx <ilya.msd2003@gmail.com>
2023-01-25 03:07:29 +0200

    refactor

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,11 +2,18 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int value;
-        int value2;
-        int variables;
-        int my_integer;
-        int my_var;
+        int variable1 = 0;
+        int variable2 = 2000;
+        int value1 = 1000;
+        int variable3 = 4000;
+        int value2 = 3000;
+        int variable4 = 5000;
+        variable4 = 7000;
+        int value3 = 90000;
+        int myInteger = 100000;
+        int myVar = 1000000;
+        int myPlusVar = 1234567;
+        int myUnderscoreInteger = 1234567;
+
     }
 }
